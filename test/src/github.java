93a1af2888d0f4ff7a72258aef0abee4d7120dcc
csc212git <145818878+csc212git@users.noreply.github.com>
2023-09-23 06:45:38 +0300

    Update github.java

--- test/src/github.java
+++ test/src/github.java
@@ -3,7 +3,9 @@ public class github {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
 		System.out.println("my name is abdullah ");
+		System.out.println("csc212");
 	}
 
 }
